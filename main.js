1f19ca11826cf0e64e3db466188fae39e382583f
Tigran <t-oganesyan00@yandex.ru>
2024-05-01 20:20:46 +0300

    изменение скритпа v2

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 window.onload = () => {
-    function makeMoveStick() {
-        const panel = document.getElementById('move');
-        const stick = document.getElementById('move__stick');
+    function makeStickDraggable(panelId, stickId) {
+        const panel = document.getElementById(panelId);
+        const stick = document.getElementById(stickId);
     
-        setInitialPosition()
         stick.addEventListener('touchstart', startDrag);
     
         function startDrag(e) {
@@ -13,72 +12,26 @@ window.onload = () => {
         }
     
         function dragStick(e) {
-            e.preventDefault()
-            const panelRect = panel.getBoundingClientRect();
-            const panelLeft = panelRect.left;
-            const panelTop = panelRect.top;
-    
-            const touch = e.touches[0];
-            const x = touch.clientX - panelLeft;
-            const y = touch.clientY - panelTop;
-    
-            setPosition(panelRect.width, panelRect.height, x, y);
-        }
-    
-        function stopDrag() {
-            document.removeEventListener('touchmove', dragStick);
-            document.removeEventListener('touchend', stopDrag);
-            setInitialPosition()
-        }
-    
-        function setPosition(panelW, panelH, x, y) {
-            var offsetX = x - stick.offsetWidth / 2;
-            var offsetY = y - stick.offsetHeight / 2;
-            offsetX = Math.max(offsetX, 0);
-            offsetY = Math.max(offsetY, 0);
-            offsetX = Math.min(offsetX, panelW - stick.offsetWidth);
-            offsetY = Math.min(offsetY, panelH - stick.offsetHeight);
-            stick.style.left = `${offsetX}px`;
-            stick.style.top = `${offsetY}px`;
-        }
-        function setInitialPosition() {
-            stick.style.left = `${panel.offsetWidth/2-stick.offsetWidth/2}px`
-            stick.style.top = `${panel.offsetHeight/2-stick.offsetHeight/2}px`
-        }
-    }
-    function makeCameraStick() {
-        const panel = document.getElementById('camera');
-        const stick = document.getElementById('camera__stick');
-    
-        setInitialPosition()
-        stick.addEventListener('touchstart', startDrag);
-    
-        function startDrag(e) {
             e.preventDefault();
-            document.addEventListener('touchmove', dragStick);
-            document.addEventListener('touchend', stopDrag);
-        }
-    
-        function dragStick(e) {
-            e.preventDefault()
             const panelRect = panel.getBoundingClientRect();
             const panelLeft = panelRect.left;
             const panelTop = panelRect.top;
     
-            const touch = e.touches[0];
+            const touch = getTouchForTarget(e, stick); 
+            if (!touch) return;
+    
             const x = touch.clientX - panelLeft;
             const y = touch.clientY - panelTop;
     
-            setPosition(panelRect.width, panelRect.height, x, y);
+            setPosition(panelRect.width, panelRect.height, x, y, stick);
         }
     
         function stopDrag() {
             document.removeEventListener('touchmove', dragStick);
             document.removeEventListener('touchend', stopDrag);
-            setInitialPosition()
         }
     
-        function setPosition(panelW, panelH, x, y) {
+        function setPosition(panelW, panelH, x, y, stick) {
             var offsetX = x - stick.offsetWidth / 2;
             var offsetY = y - stick.offsetHeight / 2;
             offsetX = Math.max(offsetX, 0);
@@ -88,11 +41,20 @@ window.onload = () => {
             stick.style.left = `${offsetX}px`;
             stick.style.top = `${offsetY}px`;
         }
-        function setInitialPosition() {
-            stick.style.left = `${panel.offsetWidth/2-stick.offsetWidth/2}px`
-            stick.style.top = `${panel.offsetHeight/2-stick.offsetHeight/2}px`
+    
+        function getTouchForTarget(event, target) {
+            const touches = event.touches;
+            for (let i = 0; i < touches.length; i++) {
+                const touch = touches[i];
+                if (touch.target === target) {
+                    return touch;
+                }
+            }
+            return null;
         }
     }
-    makeMoveStick()
-    makeCameraStick()
+    
+    makeStickDraggable('move', 'move__stick');
+    makeStickDraggable('camera', 'camera__stick');
+    
 }
